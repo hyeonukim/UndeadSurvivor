28654bbd56e3025abe42fa35c148c0df422d3ca2
Eric Kim <48634064+hyeonukim@users.noreply.github.com>
2024-01-13 16:58:45 -0800

    added EnemyDeath
    added Enemy's death
    added player's level system

--- Assets/Undead Survivor/Code/Enemy.cs
+++ Assets/Undead Survivor/Code/Enemy.cs
@@ -13,19 +13,23 @@ public class Enemy : MonoBehaviour
     bool isLive;
 
     Rigidbody2D rigid;
+    Collider2D coll;
     Animator anim;
     SpriteRenderer spriter;
+    WaitForFixedUpdate wait;
 
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
+        coll = GetComponent<Collider2D>();
         anim = GetComponent<Animator>();
         spriter = GetComponent<SpriteRenderer>();
+        wait = new WaitForFixedUpdate();
     }
 
     void FixedUpdate() 
     {
-        if (!isLive)
+        if (!isLive || anim.GetCurrentAnimatorStateInfo(0).IsName("Hit"))
             return;
 
         Vector2 dirVec = target.position - rigid.position;
@@ -44,6 +48,10 @@ public class Enemy : MonoBehaviour
     void OnEnable() {
         target = GameManager.instance.player.GetComponent<Rigidbody2D>();    
         isLive = true;
+        coll.enabled = true;
+        rigid.simulated = true;
+        spriter.sortingOrder = 2;
+        anim.SetBool("Dead", false);
         health = maxHealth;
     }
 
@@ -56,20 +64,33 @@ public class Enemy : MonoBehaviour
     }
     
     private void OnTriggerEnter2D(Collider2D other) {
-        if (!other.CompareTag("Bullet")){
+        if (!other.CompareTag("Bullet") || !isLive){
             return;
         }
 
         health -= other.GetComponent<Bullet>().damage;
+        StartCoroutine(KnockBack());
 
         if (health > 0) {
-
+            anim.SetTrigger("Hit");
         }
         else{
-            Dead();
+            isLive = false;
+            coll.enabled = false;
+            rigid.simulated = false;
+            spriter.sortingOrder = 1;
+            anim.SetBool("Dead", true);
+            GameManager.instance.kill++;
+            GameManager.instance.GetExp();
         }
     }
 
+    IEnumerator KnockBack(){
+        yield return wait;
+        Vector3 playerPos = GameManager.instance.player.transform.position;
+        Vector3 dirVec = transform.position - playerPos;
+        rigid.AddForce(dirVec.normalized * 3, ForceMode2D.Impulse);
+    }
     void Dead(){
         gameObject.SetActive(false);
     }
--- Assets/Undead Survivor/Code/GameManager.cs
+++ Assets/Undead Survivor/Code/GameManager.cs
@@ -4,9 +4,18 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager instance;
+    [Header("# Game Control")]
     public float gameTime;
     public float maxGameTime = 2 * 10f;
-    public static GameManager instance;
+
+    [Header("# Player Info")]
+    public int level;
+    public int kill;
+    public int exp;
+    public int[] nextExp = { 3, 5, 10, 100, 150, 210, 280, 360, 450, 600 };
+
+    [Header("# Game Object")]
     public PoolManager pool;
     public Player player;
 
@@ -21,6 +30,15 @@ public class GameManager : MonoBehaviour
 
         if (gameTime > maxGameTime){
             gameTime = maxGameTime;
+        }
+    }
+
+    public void GetExp(){
+        exp++;
+
+        if (exp == nextExp[level]){
+            level++;
+            exp = 0;
         }
     }
 }
