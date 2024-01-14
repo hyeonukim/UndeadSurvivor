413f9db94fda52615c280713d58159990d418263
Eric Kim <48634064+hyeonukim@users.noreply.github.com>
2024-01-13 18:24:23 -0800

    added UI
    added UI for exp, health, kill, level, timer

--- Assets/Undead Survivor/Code/GameManager.cs
+++ Assets/Undead Survivor/Code/GameManager.cs
@@ -13,6 +13,8 @@ public class GameManager : MonoBehaviour
     public int level;
     public int kill;
     public int exp;
+    public int health;
+    public int maxHealth = 100;
     public int[] nextExp = { 3, 5, 10, 100, 150, 210, 280, 360, 450, 600 };
 
     [Header("# Game Object")]
@@ -24,6 +26,9 @@ public class GameManager : MonoBehaviour
         instance = this;
     }
 
+    void Start() {
+        health = maxHealth;
+    }
     void Update()
     {
         gameTime += Time.deltaTime;
